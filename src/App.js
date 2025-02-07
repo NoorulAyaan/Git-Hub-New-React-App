7728089f4e661f0b9b1c9be2c105f9268a24af9a
Noor ul Ayaan <Ayan.cloudlem@gmail.com>
2025-02-07 22:38:28 +0000

    this is my inth commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           This is my new React App
         </p>
