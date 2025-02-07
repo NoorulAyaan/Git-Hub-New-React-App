5a0b09e817c3816a53fa885c70bd99bc01c7b6de
Noor ul Ayaan <Ayan.cloudlem@gmail.com>
2025-02-07 21:33:03 +0000

    this is my sixth push on GitHub

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,11 +1,11 @@
-import { getCLS, getFID, getLCP } from 'web-vitals';
-
-const reportWebVitals = (onPerfEntry) => {
+const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getLCP }) => {
+    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
+      getFCP(onPerfEntry);
       getLCP(onPerfEntry);
+      getTTFB(onPerfEntry);
     });
   }
 };
