f06516069237d9152fd0906debb98c1522f463a7
Noor ul Ayaan <Ayan.cloudlem@gmail.com>
2025-02-07 22:36:03 +0000

    this is my eighth commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -12,14 +11,6 @@ function App() {
         <p>
           I will create CI/CD pipelline from GitHub to GitHub Actions.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
